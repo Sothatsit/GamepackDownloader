a3dd169b7d1d8e8005024b4863651033961379b0
sothatsit <so.thatsit@hotmail.com>
2015-05-10 08:28:00 +0800

    Deciphers field descriptors

---
+++ src/net/sothatsit/gamepackdownloader/refactor/FieldDescriptor.java
@@ -0,0 +1,22 @@
+package net.sothatsit.gamepackdownloader.refactor;
+
+public class FieldDescriptor extends Descriptor {
+
+    private String type;
+
+    public FieldDescriptor(String descriptorRaw, ClassNameStore store) {
+        super(descriptorRaw);
+
+        this.type = Descriptor.getFullName(descriptorRaw, store);
+    }
+
+    @Override
+    public String getDescriptorReformatted() {
+        return null;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
