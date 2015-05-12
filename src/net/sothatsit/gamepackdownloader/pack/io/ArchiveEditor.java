b9fd2c70a1766b466baa46a8b8390d44fe5d670b
sothatsit <so.thatsit@hotmail.com>
2015-05-12 18:35:11 +0800

    add exception throwing

--- src/net/sothatsit/gamepackdownloader/pack/io/ArchiveEditor.java
+++ src/net/sothatsit/gamepackdownloader/pack/io/ArchiveEditor.java
@@ -1,12 +1,13 @@
 package net.sothatsit.gamepackdownloader.pack.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 
 public interface ArchiveEditor {
 
-    public boolean shouldEdit(File file, ZipEntry entry);
+    public boolean shouldEdit(File file, ZipEntry entry) throws IOException;
 
-    public byte[] edit(File file, ZipEntry entry, byte[] data);
+    public byte[] edit(File file, ZipEntry entry, byte[] data) throws IOException;
 
 }
--- src/net/sothatsit/gamepackdownloader/pack/io/ArchiveLoader.java
+++ src/net/sothatsit/gamepackdownloader/pack/io/ArchiveLoader.java
@@ -1,12 +1,13 @@
 package net.sothatsit.gamepackdownloader.pack.io;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.zip.ZipEntry;
 
 public interface ArchiveLoader {
 
-    public boolean shouldLoad(File file, ZipEntry entry);
+    public boolean shouldLoad(File file, ZipEntry entry) throws IOException;
 
-    public void onLoad(File file, ZipEntry entry, byte[] data);
+    public void onLoad(File file, ZipEntry entry, byte[] data) throws IOException;
 
 }
