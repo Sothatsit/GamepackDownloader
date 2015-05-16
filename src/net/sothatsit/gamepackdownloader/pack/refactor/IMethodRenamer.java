3c34dd56a9e0752da74a0796b689136e91d0a6db
sothatsit <so.thatsit@hotmail.com>
2015-05-16 10:07:21 +0800

    add more info

--- src/net/sothatsit/gamepackdownloader/pack/refactor/IMethodRenamer.java
+++ src/net/sothatsit/gamepackdownloader/pack/refactor/IMethodRenamer.java
@@ -2,6 +2,6 @@ package net.sothatsit.gamepackdownloader.pack.refactor;
 
 public interface IMethodRenamer {
 
-    public String getNewName(String className, String oldName, String descriptor);
+    public String getNewName(String className, int access, String name, String desc, String signature, String[] exceptions);
 
 }
