4fb9835fb594d69bd2596d138f0921fdd1a40a15
sothatsit <so.thatsit@hotmail.com>
2015-05-16 10:39:10 +0800

    refactor from ClassMap

--- src/net/sothatsit/gamepackdownloader/pack/refactor/RefactorMapBuilder.java
+++ src/net/sothatsit/gamepackdownloader/pack/refactor/RefactorMapBuilder.java
@@ -1,12 +1,13 @@
 package net.sothatsit.gamepackdownloader.pack.refactor;
 
-import jdk.internal.org.objectweb.asm.*;
-import net.sothatsit.gamepackdownloader.pack.io.ArchiveLoader;
 import net.sothatsit.gamepackdownloader.pack.io.JarArchive;
+import net.sothatsit.gamepackdownloader.pack.refactor.descriptor.Descriptor;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.zip.ZipEntry;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class RefactorMapBuilder {
 
@@ -21,51 +22,139 @@ public class RefactorMapBuilder {
     }
 
     public RefactorMap build(JarArchive archive) throws IOException {
-        final RefactorMap map = new RefactorMap();
+        ClassMap classMap = ClassMap.build(archive);
 
-        final ClassVisitor visitor = new ClassVisitor(Opcodes.ASM4) {
-            private String clazz;
+        RefactorMap map = new RefactorMap();
 
-            @Override
-            public void visit(int version, int access, String name, String signature, String superName, String[] interfaces) {
-                this.clazz = name;
+        for(ClassMap.MapClass clazz : classMap.getClasses()) {
+            String refactored = classRenamer.getNewName(clazz.getVersion(), clazz.getAccess(), clazz.getName(), clazz.getSignature(), clazz.getSuperName(), clazz.getInterfaces());
 
-                if(classRenamer != null) {
-                    map.setClassName(name, classRenamer.getNewName(name, interfaces, superName));
+            if(refactored.equals(clazz.getName())) {
+                continue;
+            }
+
+            map.setClassName(clazz.getName(), refactored);
+        }
+
+        for(ClassMap.MapClass clazz : classMap.getClasses()) {
+            String className = map.getNewClassName(clazz.getName());
+
+            List<ClassMap.MapClass> implementingClasses = clazz.getImplementingClasses();
+            List<ClassMap.MapClass> superMapClasses = new ArrayList<>();
+            List<Class<?>> superClasses = new ArrayList<>();
+
+            List<String> check = new ArrayList<>(Arrays.asList(clazz.getInterfaces()));
+            if(clazz.getSuperName() != null) {
+                check.add(clazz.getSuperName());
+            }
+
+            for(String clazzName : clazz.getInterfaces()) {
+                ClassMap.MapClass c = classMap.getMapClass(clazzName);
+
+                if(c != null) {
+                    superMapClasses.add(c);
+                } else {
+                    Class<?> c2 = Descriptor.getClass(clazzName);
+
+                    if(c2 != null) {
+                        superClasses.add(c2);
+                    }
                 }
             }
 
-            @Override
-            public MethodVisitor visitMethod(int access, String name, String desc, String signature, String[] exceptions) {
-                if(fieldRenamer != null) {
-                    map.setFieldName(clazz, name, fieldRenamer.getNewName(clazz, name, desc));
+            for(ClassMap.MapField field : clazz.getFields()) {
+                if(field.getName().length() > 2) {
+                    continue;
                 }
 
-                return super.visitMethod(access, name, desc, signature, exceptions);
-            }
+                String refactored = fieldRenamer.getNewName(className, field.getAccess(), field.getName(), field.getDesc(), field.getSignature(), field.getValue());
 
-            @Override
-            public FieldVisitor visitField(int access, String name, String desc, String signature, Object value) {
-                if(methodRenamer != null) {
-                    map.setMethodName(clazz, name, methodRenamer.getNewName(clazz, name, desc));
+                if(refactored.equals(field.getName())) {
+                    continue;
                 }
 
-                return super.visitField(access, name, desc, signature, value);
+                map.setFieldName(clazz.getName(), field.getName(), refactored);
             }
 
-        };
+            methods: for(ClassMap.MapMethod method : clazz.getMethods()) {
+                if(method.getName().length() > 2) {
+                    continue;
+                }
 
-        archive.loadArchive(new ArchiveLoader() {
-            @Override
-            public boolean shouldLoad(File file, ZipEntry entry) throws IOException {
-                return entry.getName().endsWith(".class");
-            }
+                for(ClassMap.MapClass mapClass : superMapClasses) {
+                    ClassMap.MapMethod m = mapClass.getMethod(method.getName());
 
-            @Override
-            public void onLoad(File file, ZipEntry entry, byte[] data) throws IOException {
-                new ClassReader(data).accept(visitor, 0);
+                    if(m != null && m.getDesc().equals(method.getDesc())) {
+                        continue methods;
+                    }
+                }
+
+                Class<?> returnType = Descriptor.getClass(method.getReturnType());
+                Class<?>[] arguments = new Class<?>[method.getArguments().length];
+
+                boolean checkClasses = true;
+                for(int i=0; i < arguments.length; i++) {
+                    arguments[i] = Descriptor.getClass(method.getArguments()[i]);
+
+                    if(arguments[i] == null) {
+                        checkClasses = false;
+                        break;
+                    }
+                }
+
+                if(checkClasses) {
+                    for(Class<?> c : superClasses) {
+                        for(Method m : c.getMethods()) {
+                            if(!m.getName().equals(method.getName()) || (returnType != null && !returnType.equals(m.getReturnType()))) {
+                                continue;
+                            }
+
+                            if(m.getParameterCount() != arguments.length) {
+                                continue;
+                            }
+
+                            for(int i=0; i < arguments.length; i++) {
+                                if(!arguments[i].equals(m.getParameterTypes()[i])) {
+                                    continue;
+                                }
+                            }
+
+                            continue methods;
+                        }
+                    }
+                }
+
+                String refactored = methodRenamer.getNewName(className, method.getAccess(), method.getName(), method.getDesc(), method.getSignature(), method.getExceptions());
+
+                if(refactored.equals(method.getName())) {
+                    continue;
+                }
+
+                map.setMethodName(className, method.getName(), refactored);
             }
-        });
+        }
+
+        for(ClassMap.MapClass clazz : classMap.getClasses()) {
+            for(ClassMap.MapMethod method : clazz.getMethods()) {
+                String newName = map.getNewMethodName(clazz.getName(), method.getName());
+
+                if(newName.equals(method.getName())) {
+                    continue;
+                }
+
+                for(ClassMap.MapClass c : clazz.getImplementingClasses()) {
+                    if(c == null) {
+                        continue;
+                    }
+
+                    ClassMap.MapMethod m = c.getMethod(method.getName());
+
+                    if(m != null && m.getDesc().equals(method.getDesc())) {
+                        map.setMethodName(c.getName(), m.getName(), newName);
+                    }
+                }
+            }
+        }
 
         map.fixDuplicates();
 
