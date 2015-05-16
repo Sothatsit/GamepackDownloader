530f9c2c93414230e2ee93076b099dd8b56774de
sothatsit <so.thatsit@hotmail.com>
2015-05-16 10:06:44 +0800

    add more info

--- src/net/sothatsit/gamepackdownloader/pack/refactor/IFieldRenamer.java
+++ src/net/sothatsit/gamepackdownloader/pack/refactor/IFieldRenamer.java
@@ -2,6 +2,6 @@ package net.sothatsit.gamepackdownloader.pack.refactor;
 
 public interface IFieldRenamer {
 
-    public String getNewName(String className, String oldName, String descriptor);
+    public String getNewName(String className, int access, String name, String desc, String signature, Object value);
 
 }
