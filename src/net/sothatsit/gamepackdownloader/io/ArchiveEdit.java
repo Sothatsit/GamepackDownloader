9af1b00426b3df8d79ae68dcb75dca598ec50447
sothatsit <so.thatsit@hotmail.com>
2015-05-17 18:50:24 +0800

    easier support for no file name changes

--- src/net/sothatsit/gamepackdownloader/io/ArchiveEdit.java
+++ src/net/sothatsit/gamepackdownloader/io/ArchiveEdit.java
@@ -5,6 +5,11 @@ public class ArchiveEdit {
     private final String fileName;
     private final byte[] contents;
 
+    public ArchiveEdit(byte[] contents) {
+        this.fileName = null;
+        this.contents = contents;
+    }
+
     public ArchiveEdit(String fileName, byte[] contents) {
         this.fileName = fileName;
         this.contents = contents;
