f577710bf46c3d142254b0fc83fadb1760edcaf4
sothatsit <so.thatsit@hotmail.com>
2015-05-17 18:49:40 +0800

    allow ArchiveEditor to return new class name as well

---
+++ src/net/sothatsit/gamepackdownloader/io/ArchiveEdit.java
@@ -0,0 +1,21 @@
+package net.sothatsit.gamepackdownloader.io;
+
+public class ArchiveEdit {
+
+    private final String fileName;
+    private final byte[] contents;
+
+    public ArchiveEdit(String fileName, byte[] contents) {
+        this.fileName = fileName;
+        this.contents = contents;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public byte[] getContents() {
+        return contents;
+    }
+
+}
