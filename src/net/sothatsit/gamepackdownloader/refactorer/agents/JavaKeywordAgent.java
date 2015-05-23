e14f1e1b5f46fd37535c3bbc47588ccebabe8c00
sothatsit <so.thatsit@hotmail.com>
2015-05-23 13:37:57 +0800

    renames classes/fields/methods from java keywords

---
+++ src/net/sothatsit/gamepackdownloader/refactorer/agents/JavaKeywordAgent.java
@@ -0,0 +1,47 @@
+package net.sothatsit.gamepackdownloader.refactorer.agents;
+
+import jdk.internal.org.objectweb.asm.tree.ClassNode;
+import jdk.internal.org.objectweb.asm.tree.FieldNode;
+import jdk.internal.org.objectweb.asm.tree.MethodNode;
+import net.sothatsit.gamepackdownloader.refactorer.RefactorAgent;
+import net.sothatsit.gamepackdownloader.refactorer.RefactorMap;
+import net.sothatsit.gamepackdownloader.util.JavaUtil;
+import net.sothatsit.gamepackdownloader.util.Log;
+
+public class JavaKeywordAgent extends RefactorAgent {
+
+    public static final String SUFFIX = "_key";
+
+    public JavaKeywordAgent(RefactorMap refactorMap) {
+        super(refactorMap);
+    }
+
+    @Override
+    public boolean accept(ClassNode classNode) {
+        return true;
+    }
+
+    @Override
+    public void refactor(ClassNode node) {
+        RefactorMap map = getRefactorMap();
+
+        if(JavaUtil.isJavaKeyword(node.name)) {
+            map.setClassName(node.name, node.name + SUFFIX);
+            Log.classRename(node, node.name + SUFFIX);
+        }
+
+        for(FieldNode field : node.fields) {
+            if(JavaUtil.isJavaKeyword(field.name)) {
+                map.setFieldName(node.name, field.name, field.name + SUFFIX);
+                Log.fieldRename(node, field, field.name + SUFFIX);
+            }
+        }
+
+        for(MethodNode method : node.methods) {
+            if(JavaUtil.isJavaKeyword(method.name)) {
+                map.setMethodName(node.name, method.name, method.name + SUFFIX);
+                Log.methodRename(node, method, method.name + SUFFIX);
+            }
+        }
+    }
+}
