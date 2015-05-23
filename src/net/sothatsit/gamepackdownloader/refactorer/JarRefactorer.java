cd458430bfd846e98ababa4ed69f2928f6643708
sothatsit <so.thatsit@hotmail.com>
2015-05-23 20:54:11 +0800

    fix exception not knowing size of zip entry

--- src/net/sothatsit/gamepackdownloader/refactorer/JarRefactorer.java
+++ src/net/sothatsit/gamepackdownloader/refactorer/JarRefactorer.java
@@ -66,7 +66,7 @@ public class JarRefactorer {
                 ClassRefactorer refactorer = new ClassRefactorer(writer, refactorMap);
                 reader.accept(refactorer, 0);
 
-                zos.putNextEntry(new ZipEntry(entry));
+                zos.putNextEntry(new ZipEntry(entry.getName()));
                 zos.write(writer.toByteArray());
 
                 Log.info("Refactored Entry \"" + entry.getName() + "\"");
