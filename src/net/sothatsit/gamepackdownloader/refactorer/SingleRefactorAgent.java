92268c7bc1ce20875ce79e84c30c7f6a31750718
sothatsit <so.thatsit@hotmail.com>
2015-05-31 08:59:33 +0800

    move utility methods to RefactorAgent

--- src/net/sothatsit/gamepackdownloader/refactorer/SingleRefactorAgent.java
+++ src/net/sothatsit/gamepackdownloader/refactorer/SingleRefactorAgent.java
@@ -1,13 +1,7 @@
 package net.sothatsit.gamepackdownloader.refactorer;
 
 import jdk.internal.org.objectweb.asm.tree.ClassNode;
-import net.sothatsit.gamepackdownloader.refactorer.agents.DuplicateMethodAgent;
-import net.sothatsit.gamepackdownloader.refactorer.agents.JavaKeywordAgent;
-import net.sothatsit.gamepackdownloader.refactorer.agents.UpperCamelCaseClassNames;
-import net.sothatsit.gamepackdownloader.util.Log;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public abstract class SingleRefactorAgent extends RefactorAgent {
@@ -35,38 +29,6 @@ public abstract class SingleRefactorAgent extends RefactorAgent {
     public abstract void logStatistics();
 
     public abstract void resetStatistics();
-
-    public static List<SingleRefactorAgent> generateAgentList(RefactorMap refactorMap) {
-        return new ArrayList<>(Arrays.asList(
-                new JavaKeywordAgent(refactorMap),
-                new DuplicateMethodAgent(refactorMap),
-                new UpperCamelCaseClassNames(refactorMap)
-        ));
-    }
-
-    public static RefactorMap refactor(List<ClassNode> classes) {
-        RefactorMap map = new RefactorMap();
-        return refactor(classes, map, generateAgentList(map));
-    }
-
-    public static RefactorMap refactor(List<ClassNode> classes, RefactorMap refactorMap, List<SingleRefactorAgent> agents) {
-        for(ClassNode node : classes) {
-            Log.clazz(node);
-            for(SingleRefactorAgent agent : agents) {
-                agent.run(node, classes);
-            }
-        }
-
-        Log.info("");
-
-        for(SingleRefactorAgent agent : agents) {
-            agent.logStatistics();
-        }
-
-        Log.info("");
-
-        return refactorMap;
-    }
 
     public boolean isSingle() {
         return true;
